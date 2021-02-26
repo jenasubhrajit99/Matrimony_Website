0d2cba84bc1e7f6e5a93c57b69729db5646788ca
unknown <jenasubhrajit99@gmail.com>
2021-02-26 19:03:50 +0530

    first  matrimony wesite add

---
+++ contact.php
@@ -0,0 +1,81 @@
+
+<!-- //header -->
+<?php require "view/mainHeader.php"?>
+ <!-- inner banner -->
+    <div class="inner_banner layer" id="home">
+        <div class="container">
+            <div class="agileinfo-inner">
+                <h2 class="text-center text-white">
+                    Contact Page
+                </h2>
+            </div>
+        </div>
+    </div>
+    <!-- //inner banner -->
+    
+    <!-- breadcrumbs -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index">Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+        </ol>
+    </nav>
+    <!-- //breadcrumbs -->
+
+<!-- contact -->
+<section class="contact py-5">
+	<div class="container py-sm-3">
+		<h3 class="heading text-capitalize mb-lg-5 mb-4"> Contact Us </h3>
+		<div class="address row">
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-map-marker"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">Address</h6>
+						<p>Bhubaneswar, Odisha 
+
+						</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-envelope"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">Email</h6>
+							<a href="tojeet98@gmail.com"> tojeet98@gmail.com</a>
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-phone"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">Phone</h6>
+						<p>+91 7978451565</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</section>	
+<!-- //contact -->
+
+<!-- footer -->	
+<jsp:include page="mainFooter.jsp"></jsp:include>
+<!-- footer -->
+
+</body>
+</html>
---
+++ view/mainFooter.php
@@ -0,0 +1,20 @@
+
+<footer>
+	<div class="container py-5">
+		<div class="row footer-gap">
+			<div class="col-lg-4 col-sm-6">
+				<h3 class="text-capitalize mb-3">Address</h3>
+				<address class="mb-0">
+					<p class=""><span class="fa fa-map-marker"></span>Locality Map. <br>BHUBANESWAR, ODISHA.</p>
+					<p><span class="fa fa-phone"></span> +91 9438346475</p>
+					<p><span class="fa fa-envelope-open"></span> <a href="tojeet98@gmail.com">tojeet98@gmail.com</a></p>
+				</address>
+			</div>
+		</div>
+	</div>
+	<div class="copyright pb-5 text-center">
+		<p>� 2018 Wedding Organizer. All Rights Reserved | Design by Subhrajit</p>
+	</div>
+</footer>
+</body>
+</html>
